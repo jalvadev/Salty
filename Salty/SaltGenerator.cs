08cfe07e88fe5856a821e0c5dd46b29f8648fabd
jalvadev <josealvarezvivar@gmail.com>
2024-04-13 10:57:05 +0200

    Added method to generate salt.

---
+++ Salty/SaltGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salty
+{
+    public class SaltGenerator
+    {
+        private static SaltGenerator _instance;
+
+        private SaltGenerator() { }
+
+        public static SaltGenerator GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SaltGenerator();
+            }
+
+            return _instance;
+        }
+
+        public string GenerateSaltString()
+        {
+            int size = 11;
+
+            byte[] salt = GenerateSalt(size);
+            return Convert.ToBase64String(salt);
+        }
+
+        private byte[] GenerateSalt(int size)
+        {
+            byte[] salt = new byte[size];
+
+            RandomNumberGenerator.Fill(salt);
+
+            return salt;
+        }
+    }
+}
