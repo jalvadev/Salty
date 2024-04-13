5207008f146275887cb4c21277e31f2ec5b84e37
jalvadev <josealvarezvivar@gmail.com>
2024-04-13 12:28:40 +0200

    Added structure for the strategy pattern.

---
+++ Salty/HashContext.cs
@@ -0,0 +1,24 @@
+﻿using Salty.Hasher;
+
+namespace Salty
+{
+    public class HashContext
+    {
+        private IHasher _hasher;
+
+        public HashContext(IHasher hasher)
+        {
+            _hasher = hasher;
+        }
+
+        public void SetHasher(IHasher hasher)
+        {
+            _hasher = hasher;
+        }
+
+        public string GenerateHash(string stringToHash)
+        {
+            return _hasher.HashString(stringToHash);
+        }
+    }
+}
---
+++ Salty/Hashers/SHA256Hasher.cs
@@ -0,0 +1,17 @@
+﻿using Salty.Hasher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salty.Hashers
+{
+    public class SHA256Hasher : IHasher
+    {
+        public string HashString(string stringToHash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
