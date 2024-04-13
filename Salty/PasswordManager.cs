46b799378db9e824f0fb0e86e1f493180a0e2c60
jalvadev <josealvarezvivar@gmail.com>
2024-04-13 18:07:38 +0200

    Added password hash generator manager and tests.

---
+++ Salty/PasswordManager.cs
@@ -0,0 +1,28 @@
+﻿using Salty.Hasher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salty
+{
+    public static class PasswordManager
+    {
+
+        public static string GeneratePasswordHash(IHasher hasher, string password)
+        {
+            string salt;
+            string hashedPassword;
+
+            var saltGenerator = SaltGenerator.GetInstance();
+            salt = saltGenerator.GenerateSaltString();
+            password += salt;
+
+            var hashContext = new HashContext(hasher);
+            hashedPassword = hashContext.GenerateHash(password);
+
+            return hashedPassword;
+        }
+    }
+}
---
+++ Tests/PasswordTests.cs
@@ -0,0 +1,47 @@
+﻿using Salty;
+using Salty.Hashers;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class PasswordTests
+    {
+        [Test]
+        public void TestPasswordsSHA256DoesntRepeat_ReturnTrue()
+        {
+            // Prepare data.
+            int timesToChek = 1000;
+            string password = "f1nd1ngN3m0_567#";
+            
+            // Get first password hashed.
+            string hashedPassword = PasswordManager.GeneratePasswordHash(new SHA256Hasher(), password);
+
+            // Assert.
+            for(int i = 0; i < timesToChek; i++)
+            {
+                string hashedPasswordToCompareWith = PasswordManager.GeneratePasswordHash(new SHA256Hasher(), password);
+                
+                Assert.That(hashedPassword != hashedPasswordToCompareWith, Is.True);
+            }
+        }
+
+        [Test]
+        public void TestPasswordsSHA512DoesntRepeat_ReturnTrue()
+        {
+            // Prepare data.
+            int timesToChek = 1000;
+            string password = "f1nd1ngN3m0_567#";
+
+            // Get first password hashed.
+            string hashedPassword = PasswordManager.GeneratePasswordHash(new SHA512Hasher(), password);
+
+            // Assert.
+            for (int i = 0; i < timesToChek; i++)
+            {
+                string hashedPasswordToCompareWith = PasswordManager.GeneratePasswordHash(new SHA512Hasher(), password);
+
+                Assert.That(hashedPassword != hashedPasswordToCompareWith, Is.True);
+            }
+        }
+    }
+}
